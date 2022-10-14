37ecfd2c757471b227847452aeb443fd1590e5cd
Khoa Huynh <huynhduykhoa1998@gmail.com>
2022-10-14 19:14:07 +0700

    first

---
+++ baitap.js
@@ -0,0 +1,123 @@
+// bai 01
+// let a = Number(prompt("Nhập số nguyên dương"));
+// for (let i = 1; i <= a; i++) {
+//     if (a % i == 0) {
+//         console.log(i);
+//     }
+// }
+
+// bai 02
+// let a = Number(prompt("Nhập số nguyên dương"));
+// for (let i = 1; i <= a; i++) {
+//     if (a % i == 0) {
+//         if (i % 2 == 0) {
+//         console.log(i);
+//         }
+//     }
+// }
+
+// bai 03
+// let a = Number(prompt("Nhập số nguyên dương"));
+// let tongUocSo = 0;
+// for (let i = 1; i <= a; i++) {
+//     if (a % i == 0) {
+//         tongUocSo += i;
+//     }
+// }
+// console.log(tongUocSo);
+
+// bai 04
+// let a = Number(prompt("Nhập số nguyên dương"));
+// let tichUocSoChan = 1;
+// for (let i = 1; i <= a; i++) {
+//     if (a % i == 0) {
+//         if (a % 2 == 0) {
+//             tichUocSoChan*a;
+//         }
+//     }
+// }
+// console.log(tichUocSoChan);
+
+// // bai 05
+// let a = Number(prompt("Nhập số nguyên dương"));
+// let demUocSo = 0;
+// for (let i = 1; i <= a; i++) {
+//     if (a % i == 0) {
+//         demUocSo++;
+//     }
+// }
+// console.log("có", demUocSo, "ước số");
+
+// // bai 06
+// let a = Number(prompt("Nhập số nguyên"));
+// let flag = 1;
+// function soNguyenTo(a) {
+//     if (a < 2) {
+//         return flag = 0;
+//     } else if (a == 2) {
+//         return flag = 1;
+//     }
+// }
+// for (let i = 1; i <= a; i++) {
+//     if (a % i == 0) {
+//         flag = 0;
+//         break;
+//     } else {
+//         flag = 1;
+//         break;
+//     }
+// }
+// if (flag = 1) {
+//     console.log("Là số nguyên tố");
+// } else {
+//     console.log("Không là số nguyên tố");
+// }
+
+
+// bai 07
+// let a = [-2, -1, 1, 3, 4, 8];
+// let n = a.length;
+// for (let i = 0; i <= n; i++) {
+//     if (a[i] % 2 == 0) {
+//         console.log(a[i]);
+//     }
+// }
+
+// bai 08
+// let a = [-2, -1, 1, 3, 4, 8];
+// let n = a.length;
+// for (let i = 0; i <= n; i++) {
+//     if (a[i] > 0) {
+//         console.log(a[i]);
+//     }
+// }
+
+// bai 10
+// let a = [-2, -1, 1, 3, 4, 8];
+// let n = a.length;
+// let tongAm = 0;
+// for (let i = 0; i <= n; i++) {
+//     if (a[i] < 0) {
+//         tongAm += a[i];
+//     }
+// }
+// console.log(tongAm);
+
+// bai 12
+// let a = [-2, -1, 1, 3, 4, 8];
+// let n = a.length;
+// let demSoLe = 0;
+// for (let i = 0; i <= n; i++) {
+//     if (!(a[i] % i == 0)) {
+//         demSoLe++;
+//     }
+// }
+// console.log("có", demSoLe, "số lẻ");
+
+// bai 14
+// let a = [-2, -1, 1, 3, 4, 8];
+// let n = a.length;
+// let index = -1;
+// for (let i = 1; i < n; i++) {
+//     if (a[i] == )
+// }
